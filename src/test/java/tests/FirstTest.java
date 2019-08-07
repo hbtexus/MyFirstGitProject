e2837e7e15d10450b2cc13035308c966dc6f1b71
Hayrulla Bekmuradov <51765331+hbtexus@users.noreply.github.com>
2019-08-06 20:51:15 -0500

    Update FirstTest.java

--- src/test/java/tests/FirstTest.java
+++ src/test/java/tests/FirstTest.java
@@ -2,7 +2,7 @@ package tests;
 
 public class FirstTest {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World with Conflict!");
         System.out.println("Hi!");
     }
 
